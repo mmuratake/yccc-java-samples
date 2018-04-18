5175244fb4f0ae0b3c50125e697bbe8127b8f97b
sMamiMuratake <sMamiMuratake@C218-1761.yccc.edu>
2018-04-18 13:44:24 -0400

    Testing to see if having getters and setters for channel etc. is better.

---
+++ src/main/java/edu/yccc/java/samples/slack/SlackMessageTest.java
@@ -0,0 +1,36 @@
+package edu.yccc.java.samples.slack;
+
+public class SlackMessageTest {
+	
+	String channel;
+	String userName;
+	String message;
+	
+	public String getChannel()
+	{
+		return channel;
+	}
+	public void setChannel(String channel) 
+	{
+		this.channel = channel;
+	}
+	public String getUserName() 
+	{
+		return userName;
+	}
+	public void setUserName(String userName) 
+	{
+		this.userName = userName;
+	}
+	public String getMessage() 
+	{
+		return message;
+	}
+	public void setMessage(String message) 
+	{
+		this.message = message;
+	}
+	
+	
+
+}
